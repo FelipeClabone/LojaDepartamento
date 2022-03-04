cab45ef26627da93f2b905554e0a5ff35aba1edc
FelipeClabone <felipe.clabone@unesp.br>
2022-03-04 19:27:18 -0300

    Strategy criados

---
+++ LojaDepartamento/src/Modelo/StrategyOrdenacaoA.java
@@ -0,0 +1,19 @@
+/*  
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Felipe de Freitas
+ */
+public class StrategyOrdenacaoA implements OrdenacaoStrategy{
+    
+    @Override
+    public void executarOrdenacao(ArrayList<Produto> produtos){
+    }
+}
